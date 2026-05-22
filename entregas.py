23dbbe6373a9985881a122c666f45aa4e81ac0fd
Pedro Henrique Tafner Reck <pedro.htr1@puccampinas.edu.br>
2026-05-22 09:31:42 -0300

    cadastro de entregadores (ainda precisa fazer algumas validações

---
+++ entregas.py
@@ -0,0 +1,33 @@
+import pedidos as p
+def entregadores(bancoEntregadores,id_pedido):
+    p.limpar()
+    opcao=0
+    while opcao != 2:
+        print("\n\t-- ENTREGADORES --")
+        print("\n1 - Cadastrar entregador")
+        print("2 - Voltar para o menu principal")
+
+        opcao=int(input("\nEscolha uma opção: "))
+
+        match opcao:
+            
+            case 1:
+                entrega=cadastrar_entregador(id_pedido)
+                id_entregador=p.gerar_id(2)
+                bancoEntregadores[id_entregador]=entrega
+                return bancoEntregadores
+            case 2:
+                return
+
+def cadastrar_entregador(id_pedido):
+    p.limpar()
+    entrega=[]
+    print("\n\t-- CADASTRO DE ENTREGADOR --")
+    nome=input("Digite o nome do entregador: ")
+    entrega.append(nome)
+    veiculo=input("Digite o veículo do entregador: ")
+    entrega.append(veiculo)
+    disponibilidade="Disponível"
+    entrega.append(id_pedido)
+    entrega.append(disponibilidade)
+    return entrega
---
+++ main.py
@@ -0,0 +1,32 @@
+import pedidos as p
+import entregas as e
+bancoPedidos={}
+bancoEntregas={}
+opcao=0
+while opcao != 4:
+    p.limpar()
+    print("\n\t-- MENU PRINCIPAL --")
+    print("\n1 - Pedidos")
+    print("2 - Entregadores")
+    print("3 - Informações")
+    print("4 - Sair")
+
+    opcao=int(input("\nEscolha uma opção: "))
+    match opcao:
+
+        case 1:
+            novoBancoPedidos,id_pedido=p.pedidos(bancoPedidos)
+            bancoPedidos.update(novoBancoPedidos)
+        case 2:     
+            if bancoPedidos == {}:
+               id_pedido=0
+            novoBancoEntregas=e.entregadores(bancoEntregas,id_pedido)
+            bancoEntregas.update(novoBancoEntregas)
+        case 3:
+            pass
+        case 4:
+            print("\nPrograma finalizado\n")
+            print(novoBancoEntregas)
+        case _:
+            p.limpar()
+
